b2fe33b5df03a73ed803c3ecabc7767c201ec307
Daniel Taylor <dan.taylor@rising-phoenix.software>
2023-03-18 20:34:34 -0400

    Adding signal handling

--- cmd/rog/rog.go
+++ cmd/rog/rog.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 var (
 	Version string = "N/A"
@@ -16,4 +21,21 @@ func printLogWelcome() {
 
 func main() {
 	printLogWelcome()
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		fmt.Println()
+		done <- true
+	}()
+
+	<-done
+	fmt.Println("exiting")
 }
