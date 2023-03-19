b58e972b77680048381fd27cc88d8c506af293f0
Daniel Taylor <dan.taylor@rising-phoenix.software>
2023-03-19 13:15:54 -0400

    Remove erroneous trap of SIGKILL

--- cmd/rog/rog.go
+++ cmd/rog/rog.go
@@ -67,7 +67,7 @@ func main() {
 
 	sigs := make(chan os.Signal, 1)
 
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	done := make(chan bool, 1)
 
